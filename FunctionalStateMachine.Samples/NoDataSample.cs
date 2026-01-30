f130a82759c909c4473e7b726423ef08a818aa9c
Lee Oades <lee.oades@gmail.com>
2026-01-30 10:38:18 +0000

    Modify Light Switch sample to use derived commands.

--- FunctionalStateMachine.Samples/NoDataSample.cs
+++ FunctionalStateMachine.Samples/NoDataSample.cs
@@ -1,24 +1,21 @@
-using FunctionalStateMachine;
 using FunctionalStateMachine.Core;
 
 namespace FunctionalStateMachine.Samples;
 
 public static class NoDataSample
 {
-    public static StateMachine<LightState, LightTrigger, LightCommand> Build()
-    {
-        return StateMachine<LightState, LightTrigger, LightCommand>.Create()
+    public static StateMachine<LightState, LightTrigger, LightCommand> Build() =>
+        StateMachine<LightState, LightTrigger, LightCommand>.Create()
             .StartWith(LightState.Off)
             .For(LightState.Off)
                 .On(LightTrigger.Toggle)
                     .TransitionTo(LightState.On)
-                    .Execute(() => new LightCommandBase("On"))
-                .For(LightState.On)
-                    .On(LightTrigger.Toggle)
-                        .TransitionTo(LightState.Off)
-                        .Execute(() => new LightCommandBase("Off"))
+                    .Execute(() => new LightCommand.SwitchOn())
+            .For(LightState.On)
+                .On(LightTrigger.Toggle)
+                    .TransitionTo(LightState.Off)
+                    .Execute(() => new LightCommand.SwitchOff())
             .Build();
-    }
 }
 
 public enum LightState
@@ -32,6 +29,8 @@ public enum LightTrigger
     Toggle
 }
 
-public abstract record LightCommand;
-
-public sealed record LightCommandBase(string Name) : LightCommand;
+public abstract record LightCommand
+{
+    public record SwitchOn : LightCommand;
+    public record SwitchOff : LightCommand;
+}
